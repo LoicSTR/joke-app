558ae58a8532bcb4b654ef462c899d393a47d88d
Loïc Souêtre <loic.soutre@sfr.fr>
2025-03-26 12:40:09 +0100

    Gérer le form pour enregistrer l'auteur de la blague

--- src/routes/form/+page.server.ts
+++ src/routes/form/+page.server.ts
@@ -2,16 +2,18 @@ import { addJoke } from '$lib/server/db/joke.js';
 import { fail } from '@sveltejs/kit';
 
 export const actions = {
-	validation: async ({ request }) => {
+	validation: async ({ request, url }) => {
 		const data = await request.formData();
 		const id = Date.now();
 		const joke = data.get('joke')?.toString();
 		const answer = data.get('answer')?.toString();
 		const type = data.get('type')?.toString();
-		const author = 'Loïc';
+		const author = url.searchParams.get('user')?.toString() ?? '';
 
-		if (!joke || !answer || !type)
-			return fail(400, { message: "Il manque un élément de la blague'", field: 'joke' });
+		if (!joke) return fail(400, { message: "Il manque l'intitulé de la blague'", field: 'joke' });
+		if (!answer)
+			return fail(400, { message: "Il manque la réponse à la blague'", field: 'answer' });
+		if (!type) return fail(400, { message: "Il manque le type de la blague'", field: 'type' });
 
 		try {
 			await addJoke({ id, joke, answer, type, author });
--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,32 +1,41 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import Button from '$lib/components/Button.svelte';
-	const { form } = $props();
+	const { form, data } = $props();
 </script>
 
 <section class="form-container">
-	<Button text="Retourner à l'accueil" link="/"></Button>
-	{#if form?.success}
-		<p>Ta blague vient d'être ajouté !</p>
-	{/if}
-	<form action="?/validation" method="POST" use:enhance>
-		<label>
+	<form action="?/validation&user={data.user?.name}" method="POST" use:enhance>
+		<Button text="Retourner à l'accueil" link="/"></Button>
+		<label class:error={form?.field === 'joke'}>
 			Intitulé de la blague
-			<input type="text" name="joke" />
+			<textarea name="joke"></textarea>
+			{#if form?.message && form?.field === 'joke'}
+				<p class="error-message">{form.message}</p>
+			{/if}
 		</label>
-		<label>
+		<label class:error={form?.field === 'answer'}>
 			Réponse de la blague
-			<input type="text" name="answer" />
+			<textarea name="answer"></textarea>
+			{#if form?.message && form?.field === 'answer'}
+				<p class="error-message">{form.message}</p>
+			{/if}
 		</label>
-		<label>
+		<label class:error={form?.field === 'type'}>
 			Type de la blague
 			<select name="type">
 				<option value="global">Basique</option>
 				<option value="dev">Geek</option>
 				<option value="dark">Sombre</option>
 			</select>
+			{#if form?.message && form?.field === 'type'}
+				<p class="error-message">{form.message}</p>
+			{/if}
 		</label>
-		<button>Ajouter la blague</button>
+		<Button text="Ajouter la blague"></Button>
+		{#if form?.success}
+			<p>Ta blague vient d'être ajouté !</p>
+		{/if}
 	</form>
 </section>
 
@@ -35,5 +44,26 @@
 		display: flex;
 		flex-direction: column;
 		gap: 1rem;
+		width: 22rem;
+	}
+
+	.error-message {
+		color: red;
+		font-style: italic;
+	}
+
+	.error textarea,
+	.error select {
+		border: solid 1px red;
+	}
+
+	label {
+		display: flex;
+		flex-direction: column;
+	}
+
+	textarea,
+	select {
+		margin-top: 0.25rem;
 	}
 </style>
